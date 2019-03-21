8708562b60be89c37936cddcf6495c54a783a323
Pere Miquel Brull <peremiquelbrull@gmail.com>
2019-03-21 20:18:37 +0100

    Fix properties syntax

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,8 +3,8 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 import sbt._
 
 object Dependencies {
-  val udashCoreVersion = $udash_version$
-  val udashJQueryVersion = $udash_jquery_version$
+  val udashCoreVersion = "$udash_version$"
+  val udashJQueryVersion = "$udash_jquery_version$"
 
   val frontendDeps = Def.setting(Seq[ModuleID](
     "io.udash" %%% "udash-core-frontend" % udashCoreVersion,
