bd81238a9ab0de9b3487afc9467e97f833d11e2f
Pere Miquel Brull <peremiquelbrull@gmail.com>
2019-03-24 10:12:58 +0100

    Fix $ messing with g8

--- src/main/g8/src/main/scala/$package$/states.scala
+++ src/main/g8/src/main/scala/$package$/states.scala
@@ -5,8 +5,7 @@ import io.udash._
 sealed abstract class RoutingState(val parentState: Option[ContainerRoutingState]) extends State {
   type HierarchyRoot = RoutingState
 
-  def url(implicit application: Application[RoutingState]): String =
-    s"#${application.matchState(this).value}"
+  def url(implicit application: Application[RoutingState]): String = "#" + application.matchState(this).value
 }
 
 sealed abstract class ContainerRoutingState(parentState: Option[ContainerRoutingState]) extends RoutingState(parentState) with ContainerState
